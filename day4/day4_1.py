541e06bf591f2b06a14336eb51a51698ab192c61
tod3496 <tod3496@rit.edu>
2024-12-04 11:14:08 -0500

    I made day4_1 less ugly

--- day4/day4_1.py
+++ day4/day4_1.py
@@ -1,3 +1,5 @@
+import re
+
 DATAFILE = 'day4.data'
 UP = -1
 DOWN = 1
@@ -10,69 +12,65 @@ with open(DATAFILE) as f:
         LINES.append(line.strip())
 
 
-def search_line(lines: list[str], start_row, start_col, row_incr, col_incr):
+def get_line(lines: list[str], start_row, start_col, row_incr, col_incr):
     row = start_row
     col = start_col
 
-    working_string = ''
-    count = 0
+    string = ''
 
     while 0 <= row < len(lines) and 0 <= col < len(lines[row]):
-        working_string += lines[row][col]
-        if 'XMAS' in working_string:
-            working_string = ''
-            count += 1
+        string += lines[row][col]
         row += row_incr
         col += col_incr
+    
+    return string
 
+
+def seperate_directions(lines):
+    '''
+    takes a 2d array of characters and returns all of the
+    horizontals, verticals, and diagonals as strings
+    '''
+    horizontals = []
+    for r in range(len(lines)):
+        horizontals.append(get_line(lines, r, 0, 0, RIGHT))
+
+    verticals = []
+    for c in range(len(lines[0])):
+        verticals.append(get_line(lines, 0, c, DOWN, 0))
+
+    right_diagonals = []
+    for r in range(len(LINES)):
+        right_diagonals.append(get_line(LINES, r, 0, DOWN, RIGHT))
+    for c in range(1, len(LINES[0])):
+        right_diagonals.append(get_line(LINES, 0, c, DOWN, RIGHT))
+
+    left_diagonals = []
+    for r in range(len(LINES)):
+        left_diagonals.append(get_line(LINES, r, len(LINES[r]) - 1, DOWN, LEFT))
+    for c in range(0, len(LINES[0]) - 1):
+        left_diagonals.append(get_line(LINES, 0, c, DOWN, LEFT))
+
+    return horizontals, verticals, right_diagonals, left_diagonals
+
+
+def search_string(string):
+    count = 0
+    count += len(re.findall('XMAS', string))
+    count += len(re.findall('SAMX', string))
     return count
 
 
 def main():
     count = 0
 
-    # left to right
-    for r in range(len(LINES)):
-        count += search_line(LINES, r, 0, 0, RIGHT)
-
-    # right to left
-    for r in range(len(LINES)):
-        count += search_line(LINES, r, len(LINES[r]) - 1, 0, LEFT)
-
-    # top to bottom
-    for c in range(len(LINES[0])):
-        count += search_line(LINES, 0, c, DOWN, 0)
-
-    # bottom to top
-    for c in range(len(LINES[0])):
-        count += search_line(LINES, len(LINES) - 1, c, UP, 0)
-
-    # diagonal top left to bottom right
-    for r in range(len(LINES)):
-        count += search_line(LINES, r, 0, DOWN, RIGHT)
-    for c in range(1, len(LINES[0])):
-        count += search_line(LINES, 0, c, DOWN, RIGHT)
-
-    # diagonal bottom right to top left
-    for r in range(len(LINES)):
-        count += search_line(LINES, r, len(LINES[r]) - 1, UP, LEFT)
-    for c in range(0, len(LINES[0]) - 1):
-        count += search_line(LINES, len(LINES) - 1, c, UP, LEFT)
-
-    # diagonal top right to bottom left
-    for r in range(len(LINES)):
-        count += search_line(LINES, r, len(LINES[r]) - 1, DOWN, LEFT)
-    for c in range(0, len(LINES[0]) - 1):
-        count += search_line(LINES, 0, c, DOWN, LEFT)
-
-    # diagonal bottom left to top right
-    for r in range(len(LINES)):
-        count += search_line(LINES, r, 0, UP, RIGHT)
-    for c in range(1, len(LINES[0])):
-        count += search_line(LINES, len(LINES) - 1, c, UP, RIGHT)
+    for direction in seperate_directions(LINES):
+        for string in direction:
+            count += search_string(string)
 
     print(count)
 
 
 if __name__ == '__main__':
     main()
+    
